3bff224d3fb6369dc4b9146539a3ab0862be7558
JacekBlazejak <jacek-blazejak@o2.pl>
2026-02-08 11:50:35 +0100

    Zadanie domowe 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Jacek";
+var age = 17;
+string sex = "male";
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine(("Koobieta poniżej 30 lat."));
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (age < 18 && sex == "male")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
